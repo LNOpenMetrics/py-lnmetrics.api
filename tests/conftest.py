e4deadf4e3996531b335688ecb5d53c16ec9822e
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-03-21 22:31:01 +0100

    feat(api): add the possibility to run an sync client
    Changelog-Added: add the possibility to run an sync client
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,8 @@ from lnmetrics_api import LNMetricsClient
 @pytest.fixture(scope="session", autouse=True)
 def client():
     return LNMetricsClient(service_url="https://api.lnmetrics.info/query")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def sync_client():
+    return LNMetricsClient(service_url="https://api.lnmetrics.info/query", sync=True)
